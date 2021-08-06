6a1723c87e148f5476760f95043153631a318179
Miryam <mbathilde@gmail.com>
2021-08-06 14:55:30 +0200

    page loader compleated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+window.addEventListener("load", () => {
+	document.querySelector(".main").classList.remove("hidden");
+	document.querySelector(".home-section").classList.add("active");
+	/* page loader */
+	document.querySelector(".page-loader").classList.add("fade-out");
+	setTimeout(() => {
+		document.querySelector(".page-loader").style.display = "none";
+	}, 600);
+});
+
 /* --------- Toggle Navbar ---------- */
 const navToggler = document.querySelector(".nav-toggler");
 navToggler.addEventListener("click", () => {
