937c7585f92eb09f3c8178ce48fc3b4b56f370b4
Miryam <mbathilde@gmail.com>
2021-08-06 13:29:25 +0200

    navbar, overlay and created links to section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+/* --------- Toggle Navbar ---------- */
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+	hideSection();
+	toggleNavbar();
+	document.body.classList.toggle("hide-srolling");
+});
+
+function hideSection() {
+	document.querySelector("section.active").classList.toggle("fade-out");
+}
+
+function toggleNavbar() {
+	document.querySelector(".header").classList.toggle("active");
+}
+
+/* ---- Active Section -------- */
+
+document.addEventListener("click", (e) => {
+	if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+		// activate the overlay to prevent multiple clicks //
+		document.querySelector(".overlay").classList.add("active");
+		navToggler.classList.add("hide");
+		if (e.target.classList.contains("nav-item")) {
+			toggleNavbar();
+			document.body.classList.add("hide-scrolling");
+		}
+		else {
+			hideSection();
+		}
+		setTimeout(() => {
+			document.querySelector("section.active").classList.remove("active", "fade-out");
+			document.querySelector(e.target.hash).classList.add("active");
+			window.scrollTo(0, 0);
+			document.body.classList.remove("hide-scrolling");
+			navToggler.classList.remove("hide");
+			document.querySelector(".overlay").classList.remove("active");
+		}, 500);
+	}
+});
+
+
 /* ---- About Tabs -------- */
 
 const tabsContainer = document.querySelector(".about-tabs"),
@@ -46,3 +88,5 @@ function portfolioItemsDetails(portfolioItem) {
 
 	document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 }
+
+
